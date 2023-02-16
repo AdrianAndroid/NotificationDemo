90b4140ac091db68342954469ce3c26a52bb624e
zhaojian <zhaojian@apusapps.com>
2023-02-16 09:23:30 +0800

    增加权限弹窗

--- app/src/main/java/com/notification/demo/MainActivity.kt
+++ app/src/main/java/com/notification/demo/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.notification.demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()) { granted ->
+        if (granted) {
+            // User allow the permission.
+        } else {
+            // User deny the permission.
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.requestPermission).setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 33) {
+                requestPermissionLauncher.launch(POST_NOTIFICATIONS)
+            }
+        }
     }
+
+    // NotificationManager.areNotificationsEnabled()+.
+
+    private fun jumpNotificationSetting() {
+        val applicationInfo = applicationInfo
+        try {
+            val intent = Intent()
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
+            intent.putExtra("app_package", applicationInfo.packageName)
+            intent.putExtra("android.provider.extra.APP_PACKAGE", applicationInfo.packageName)
+            intent.putExtra("app_uißd", applicationInfo.uid)
+            startActivity(intent)
+        } catch (t: Throwable) {
+            t.printStackTrace()
+            val intent = Intent()
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.action = "android.settings.APPLICATION_DETAILS_SETTINGS"
+            intent.data = Uri.fromParts("package", applicationInfo.packageName, null)
+            startActivity(intent)
+        }
+    }
+
 }
